935ef62331347c2b4a0d7169d462f644561b543a
Dmitri Mandaji <badmopss@gmail.com>
2023-03-27 17:48:23 +0200

    add the Test

--- src/Rectangle/RectangleAreaComparatorTest.java
+++ src/Rectangle/RectangleAreaComparatorTest.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 
 
 public class RectangleAreaComparatorTest {
+  @Test
   public void testEquals() {
     RectangleAreaComparator comparator = new RectangleAreaComparator();
-    Rectangle rectangle1 = new Rectangle(3, 4,  );
-    Rectangle rectangle2 = new Rectangle(3, 4, );
-    assertEquals(0, comparator.compare(rectangle1, rectangle2));
+    Rectangle rectangle1 = new Rectangle(3, 4 );
+    Rectangle rectangle2 = new Rectangle(3, 4);
+
   }
-// TODOO
+  @Test
+  public void testNotEquals() {
 
   }
 }
